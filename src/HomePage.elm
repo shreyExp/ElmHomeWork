85dbddfbcdec7414afe23c3937ede07e3bee50f6
Shreyansh Singh <shrey543210@gmail.com>
2022-02-09 16:31:56 +0000

    click functionality added

--- src/HomePage.elm
+++ src/HomePage.elm
@@ -3,32 +3,88 @@ module HomePage exposing (main)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Browser
 
-main =
-    view "dummy model" -- the model that we pass to view function is "dummy model"
+
+main = 
+    Browser.sandbox
+    {
+         init = initialModel
+        ,view = view
+        ,update = update
+    }
 
 -- put the super container in the model
 view model =
-    level_1
+    level_1 model
 
-level_1 = div [class "level_1_container"][ category_one_container, other_categories_container ]
+type alias Model = List Category
+initialModel : Model
+initialModel =
+    [
+        categoryOne,
+        categoryTwo,
+        categoryThree,
+        categoryFour
+    ]
 
-category_one_container = div category_1_properties [paragraph, dropdown]
-other_categories_container = div [class "level_2_container"] other_categories
-other_categories = [category_two, category_three, category_four]
+-- type Msg = Clicked String| NotClicked String
+type alias Msg = {name: String}
+update : Msg -> Model -> Model
+update msg model =
+    List.map (respond msg) model
 
-type Msg = Increment | Decrement
+respond : Msg -> Category -> Category
+respond msg cat =
+    if msg.name == cat.name then
+        Category cat.name (not cat.dropdownStatus) cat.options (makeText cat.dropdownStatus)
+    else
+        cat
 
-dropdown = button [onClick Decrement] [text "Click me"]
-category_two = div category_2_properties [paragraph]
-category_three = div category_3_properties [paragraph]
-category_four = div category_4_properties [paragraph]
+makeText : Bool -> String
+makeText status =
+    if status then
+        ""
+    else
+        "Hello"
+        
 
 
-category_1_properties = [class "level_2_container", id "category_one", margin_style, margin_bottom_style, height_style, border_style]
-category_2_properties = [class "level_3_container", border_style, margin_style, height_style]
-category_3_properties = [class "level_4_container", half_width_style, inline_style, border_style, margin_style, height_style]
-category_4_properties = [class "level_4_container", half_width_style, inline_style, border_style, margin_style, height_style]
+type alias Option = {status: Bool, text: String}
+type alias Options = List Option
+type alias Category = {name: String, dropdownStatus: Bool, options: Options, text: String}
+
+categoryOne = Category "CategoryOne" False options ""
+categoryTwo = Category "CategoryTwo" False options ""
+categoryThree = Category "CategoryThree" False options ""
+categoryFour = Category "CategoryFour" False options ""
+
+options : Options
+options = List.map makeOption (List.range 1 10)
+makeOption : Int -> Option
+makeOption num = Option False ("Option " ++ (String.fromInt num))
+
+level_1 model = div [class "level_1_container"] (List.map makeCategory model)
+
+makeCategory x =
+        div divStyle [ 
+                         h1 [] [text x.name]
+                        ,button [onClick (Msg x.name)] [text "Options"]
+                        ,div [] (showOptions x.options)
+                        ,p [] [text x.text]
+
+                     ]
+
+-- showOptions : Options -> List Html.html
+showOptions option_s =
+    List.map makeParagraphFromOption option_s
+
+-- makeParagraphFromOption : Option -> Html.html
+makeParagraphFromOption option =
+    p [] [text option.text]
+
+divStyle = [margin_style, border_style, height_style]
+
 
 margin_style = style "margin" "5px"
 margin_bottom_style = style "margin-bottom" "30px"
